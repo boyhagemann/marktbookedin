cc4911a612033d78e5dd00d6588b687515a0c671
boyhagemann <boyhagemann@gmail.com>
2014-07-01 13:43:24 +0200

    update navbar

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -4,14 +4,6 @@
         @include('partials.navbar')
         @include('partials.search')
 
-        @if(Auth::check())
-        Welkom {{ Auth::user()->name }}
-        <a href="{{ URL::route('auth.logout')  }}">Log out</a>
-
-        @else
-        <a href="{{ URL::route('auth.social', ['strategy' => 'linkedin'])  }}">LinkedIn</a>
-        @endif
-
         <h3>{{ Lang::get('home.advertisements.title') }}</h3>
         @include('advertisements.list')
 
--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -3,3 +3,13 @@
     <a href="{{ URL::to('/') }}">{{ Lang::get('navigation.home') }}</a>
     <a href="{{ URL::route('advertisements.index') }}">{{ Lang::get('navigation.advertisements') }}</a>
 </nav>
+
+<nav>
+    @if(Auth::check())
+    Welkom {{ Auth::user()->name }}
+    <a href="{{ URL::route('auth.logout')  }}">Log out</a>
+
+    @else
+    <a href="{{ URL::route('auth.social', ['strategy' => 'linkedin'])  }}">Login via LinkedIn</a>
+    @endif
+</nav>
